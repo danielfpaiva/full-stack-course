14739f603e9d0a38205fef79b4c08012ec903707
danielfpaiva <danielfonsecapaiva@gmail.com>
2017-12-20 23:53:34 +0000

    HTML and start js

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Paiva on 20/12/2017.
+ */
+var firstName = prompt('What is your first name?');
+var lastName = prompt("What is your surname?");
+var age = prompt("How old are you?");
+
+alert("Nice to meet you, "+firstName+" "+lastName);
+alert("Good, You have "+age+" years old.");
